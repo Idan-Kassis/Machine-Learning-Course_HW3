29f6869dba3735127f929a938bc013802cf4d2b6
Idan-Kassis <93663187+Idan-Kassis@users.noreply.github.com>
2022-06-27 16:46:53 +0300

    Add files via upload

---
+++ main_EX1.py
@@ -0,0 +1,442 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 18:59:01 2022
+
+@author: Dror
+"""
+import matplotlib.pyplot as plt
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from ANN_perceptron import NeuralNetMLP_1h, NeuralNetMLP_2h
+import numpy as np
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import to_categorical
+import pandas as pd
+from sklearn import metrics
+
+# Parameters
+num_epochs = 400
+minibatch_size = 100
+
+# %% ---------------------- EX. 1 - ANN from Scratch --------------------------
+# Preparing MNIST dataset
+
+X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
+X = X.values
+y = y.astype(int).values
+X = ((X / 255.) - .5) * 2
+
+#  Visualize
+# all classes
+fig, ax = plt.subplots(nrows=2, ncols=5, sharex=True, sharey=True)
+ax = ax.flatten()
+for i in range(10):
+    img = X[y == i][0].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+
+# version of one class
+plt.show()
+fig, ax = plt.subplots(nrows=5, ncols=5, sharex=True, sharey=True)
+ax = ax.flatten()
+for i in range(25):
+    img = X[y == 7][i].reshape(28, 28)
+    ax[i].imshow(img, cmap='Greys')
+ax[0].set_xticks([])
+ax[0].set_yticks([])
+plt.tight_layout()
+plt.show()
+
+#  Data Spliting
+X_temp, X_test, y_temp, y_test = train_test_split(
+    X, y, test_size=35000, random_state=123, stratify=y)
+
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_temp, y_temp, test_size=21000, random_state=123, stratify=y_temp)
+
+# optional to free up some memory by deleting non-used arrays:
+del X_temp, y_temp, X, y
+
+# %% Functions
+
+def minibatch_generator(X, y, minibatch_size):
+    indices = np.arange(X.shape[0])
+    np.random.shuffle(indices)
+
+    for start_idx in range(0, indices.shape[0] - minibatch_size 
+                           + 1, minibatch_size):
+        batch_idx = indices[start_idx:start_idx + minibatch_size]
+        
+        yield X[batch_idx], y[batch_idx]
+        
+def int_to_onehot(y, num_labels):
+
+    ary = np.zeros((y.shape[0], num_labels))
+    for i, val in enumerate(y):
+        ary[i, val] = 1
+
+    return ary
+
+def mse_loss(targets, probas, num_labels=10):
+    onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+    return np.mean((onehot_targets - probas)**2)
+
+
+def accuracy(targets, predicted_labels):
+    return np.mean(predicted_labels == targets) 
+
+
+def compute_mse_and_acc(nnet, X, y, num_labels=10, minibatch_size=100):
+    mse, correct_pred, num_examples = 0., 0, 0
+    minibatch_gen = minibatch_generator(X, y, minibatch_size)
+    prob_classes = []
+    for i, (features, targets) in enumerate(minibatch_gen):
+
+        _, _, probas = nnet.forward(features)
+        predicted_labels = np.argmax(probas, axis=1)
+        
+        onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+        loss = np.mean((onehot_targets - probas)**2)
+        correct_pred += (predicted_labels == targets).sum()
+        
+        num_examples += targets.shape[0]
+        mse += loss
+        prob_classes.append(probas)
+    mse = mse/i
+    acc = correct_pred/num_examples
+    return mse, acc, prob_classes
+
+
+def train(model, X_train, y_train, X_valid, y_valid, num_epochs,
+          learning_rate=0.1):
+    
+    epoch_loss = []
+    epoch_train_acc = []
+    epoch_valid_acc = []
+    
+    for e in range(num_epochs):
+
+        # iterate over minibatches
+        minibatch_gen = minibatch_generator(
+            X_train, y_train, minibatch_size)
+
+        for X_train_mini, y_train_mini in minibatch_gen:
+            
+            #### Compute outputs ####
+            a_h1, a_h2, a_out = model.forward(X_train_mini)
+
+            #### Compute gradients ####
+            d_loss__d_w_out, d_loss__d_b_out,  d_loss__d_w_h2, d_loss__d_b_h2, d_loss__d_w_h1, d_loss__d_b_h1 = \
+                model.backward(X_train_mini, a_h1, a_h2, a_out, y_train_mini)
+
+            #### Update weights ####
+            model.weight_h1 -= learning_rate * d_loss__d_w_h1
+            model.bias_h1 -= learning_rate * d_loss__d_b_h1
+            model.weight_h2 -= learning_rate * d_loss__d_w_h2
+            model.bias_h2 -= learning_rate * d_loss__d_b_h2
+            model.weight_out -= learning_rate * d_loss__d_w_out
+            model.bias_out -= learning_rate * d_loss__d_b_out
+        
+        #### Epoch Logging ####        
+        train_mse, train_acc,_ = compute_mse_and_acc(model, X_train, y_train)
+        valid_mse, valid_acc,_ = compute_mse_and_acc(model, X_valid, y_valid)
+        train_acc, valid_acc = train_acc*100, valid_acc*100
+        epoch_train_acc.append(train_acc)
+        epoch_valid_acc.append(valid_acc)
+        epoch_loss.append(train_mse)
+        print(f'Epoch: {e+1:03d}/{num_epochs:03d} '
+              f'| Train MSE: {train_mse:.2f} '
+              f'| Train Acc: {train_acc:.2f}% '
+              f'| Valid Acc: {valid_acc:.2f}%')
+
+    return epoch_loss, epoch_train_acc, epoch_valid_acc
+
+#%% ------------- First Model - 2 Hidden layers from scratch -----------------
+model_2h = NeuralNetMLP_2h(num_features=28*28,
+                     num_hidden=50,
+                     num_classes=10)
+
+# iterate over training epochs
+for i in range(num_epochs):
+
+    # iterate over minibatches
+    minibatch_gen = minibatch_generator(
+        X_train, y_train, minibatch_size)
+    
+    for X_train_mini, y_train_mini in minibatch_gen:
+
+        break
+        
+    break
+
+_, _, probas = model_2h.forward(X_valid)
+mse = mse_loss(y_valid, probas)
+
+predicted_labels = np.argmax(probas, axis=1)
+acc = accuracy(y_valid, predicted_labels)
+print(f'Initial validation MSE: {mse:.1f}')
+print(f'Initial validation accuracy: {acc*100:.1f}%')
+
+mse, acc,_ = compute_mse_and_acc(model_2h, X_valid, y_valid)
+print(f'Initial valid MSE: {mse:.1f}')
+print(f'Initial valid accuracy: {acc*100:.1f}%')
+
+# Training session
+np.random.seed(123) # for the training set shuffling
+
+epoch_loss, epoch_train_acc, epoch_valid_acc = train(
+    model_2h, X_train, y_train, X_valid, y_valid,
+    num_epochs=num_epochs, learning_rate=0.1)
+
+# Evaluation
+plt.plot(range(len(epoch_loss)), epoch_loss)
+plt.ylabel('Mean squared error')
+plt.xlabel('Epoch')
+plt.show()
+
+plt.plot(range(len(epoch_train_acc)), epoch_train_acc,
+         label='Training')
+plt.plot(range(len(epoch_valid_acc)), epoch_valid_acc,
+         label='Validation')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(loc='lower right')
+plt.show()
+
+test_mse, test_acc , prob_classes= compute_mse_and_acc(model_2h, X_test, y_test)
+print(f'Test accuracy: {test_acc*100:.2f}%')
+
+y_pred = prob_classes
+y_pred = np.array(y_pred)
+y_pred = y_pred.reshape([35000,10])
+macro_auc = metrics.roc_auc_score(to_categorical(y_test),y_pred, multi_class="ovr")
+print('Test Macro-AUC - ', macro_auc)
+#%%
+
+def compute_mse_and_acc1(nnet, X, y, num_labels=10, minibatch_size=100):
+    mse, correct_pred, num_examples = 0., 0, 0
+    minibatch_gen = minibatch_generator(X, y, minibatch_size)
+        
+    for i, (features, targets) in enumerate(minibatch_gen):
+
+        _, probas = nnet.forward(features)
+        predicted_labels = np.argmax(probas, axis=1)
+        
+        onehot_targets = int_to_onehot(targets, num_labels=num_labels)
+        loss = np.mean((onehot_targets - probas)**2)
+        correct_pred += (predicted_labels == targets).sum()
+        
+        num_examples += targets.shape[0]
+        mse += loss
+
+    mse = mse/i
+    acc = correct_pred/num_examples
+    return mse, acc
+
+def train1(model, X_train, y_train, X_valid, y_valid, num_epochs,
+          learning_rate=0.1):
+    
+    epoch_loss = []
+    epoch_train_acc = []
+    epoch_valid_acc = []
+    
+    for e in range(num_epochs):
+
+        # iterate over minibatches
+        minibatch_gen = minibatch_generator(
+            X_train, y_train, minibatch_size)
+
+        for X_train_mini, y_train_mini in minibatch_gen:
+            
+            #### Compute outputs ####
+            a_h, a_out = model.forward(X_train_mini)
+
+            #### Compute gradients ####
+            d_loss__d_w_out, d_loss__d_b_out, d_loss__d_w_h, d_loss__d_b_h = \
+                model.backward(X_train_mini, a_h, a_out, y_train_mini)
+
+            #### Update weights ####
+            model.weight_h -= learning_rate * d_loss__d_w_h
+            model.bias_h -= learning_rate * d_loss__d_b_h
+            model.weight_out -= learning_rate * d_loss__d_w_out
+            model.bias_out -= learning_rate * d_loss__d_b_out
+        
+        #### Epoch Logging ####        
+        train_mse, train_acc = compute_mse_and_acc1(model, X_train, y_train)
+        valid_mse, valid_acc = compute_mse_and_acc1(model, X_valid, y_valid)
+        train_acc, valid_acc = train_acc*100, valid_acc*100
+        epoch_train_acc.append(train_acc)
+        epoch_valid_acc.append(valid_acc)
+        epoch_loss.append(train_mse)
+        print(f'Epoch: {e+1:03d}/{num_epochs:03d} '
+              f'| Train MSE: {train_mse:.2f} '
+              f'| Train Acc: {train_acc:.2f}% '
+              f'| Valid Acc: {valid_acc:.2f}%')
+
+    return epoch_loss, epoch_train_acc, epoch_valid_acc
+#%% ------------- second Model - 1 Hidden layers from scratch -----------------
+model_1h = NeuralNetMLP_1h(num_features=28*28,
+                     num_hidden=50,
+                     num_classes=10)
+
+# iterate over training epochs
+for i in range(num_epochs):
+
+    # iterate over minibatches
+    minibatch_gen = minibatch_generator(
+        X_train, y_train, minibatch_size)
+    
+    for X_train_mini, y_train_mini in minibatch_gen:
+
+        break
+        
+    break
+
+_,  probas = model_1h.forward(X_valid)
+mse = mse_loss(y_valid, probas)
+
+predicted_labels = np.argmax(probas, axis=1)
+acc = accuracy(y_valid, predicted_labels)
+print(f'Initial validation MSE: {mse:.1f}')
+print(f'Initial validation accuracy: {acc*100:.1f}%')
+
+mse, acc = compute_mse_and_acc1(model_1h, X_valid, y_valid)
+print(f'Initial valid MSE: {mse:.1f}')
+print(f'Initial valid accuracy: {acc*100:.1f}%')
+
+# Training session
+np.random.seed(123) # for the training set shuffling
+
+epoch_loss, epoch_train_acc, epoch_valid_acc = train1(
+    model_1h, X_train, y_train, X_valid, y_valid,
+    num_epochs=num_epochs, learning_rate=0.1)
+
+# Evaluation
+plt.plot(range(len(epoch_loss)), epoch_loss)
+plt.ylabel('Mean squared error')
+plt.xlabel('Epoch')
+plt.show()
+
+plt.plot(range(len(epoch_train_acc)), epoch_train_acc,
+         label='Training')
+plt.plot(range(len(epoch_valid_acc)), epoch_valid_acc,
+         label='Validation')
+plt.ylabel('Accuracy')
+plt.xlabel('Epochs')
+plt.legend(loc='lower right')
+plt.show()
+
+test_mse, test_acc = compute_mse_and_acc1(model_1h, X_test, y_test)
+print(f'Test accuracy: {test_acc*100:.2f}%')
+
+# %% Third model - keras 1 Hidden layer
+num_epochs = 100
+
+y_train = pd.Categorical(y_train)
+y_train = to_categorical(y_train)
+
+y_valid = pd.Categorical(y_valid)
+y_valid = to_categorical(y_valid)
+
+y_test = pd.Categorical(y_test)
+y_test = to_categorical(y_test)
+
+model_keras_1h = keras.Sequential()
+model_keras_1h.add(layers.Dense(50, activation="sigmoid"))
+model_keras_1h.add(layers.Dense(10, activation="softmax"))
+
+model_keras_1h.compile(optimizer=keras.optimizers.Adam(lr = 0.001), 
+                       loss='categorical_crossentropy',
+                       metrics=['accuracy'])
+
+# Train
+history = model_keras_1h.fit(X_train, y_train, 
+                             batch_size=minibatch_size, epochs=num_epochs,
+                             validation_data=(X_valid, y_valid))
+
+# Visualization
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+        
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+# Evaluation
+accuracy_test = model_keras_1h.evaluate(X_test, y_test)
+print('Test Accuracy - ', accuracy_test[1])
+
+y_pred = model_keras_1h.predict(X_test)
+macro_auc = metrics.roc_auc_score(y_test,y_pred)
+print('Test Macro-AUC - ', macro_auc)
+
+# %% Fourth model - keras 2 Hidden layer
+
+model_keras_2h = keras.Sequential()
+model_keras_2h.add(layers.Dense(50, activation="sigmoid"))
+model_keras_2h.add(layers.Dense(50, activation="sigmoid"))
+model_keras_2h.add(layers.Dense(10, activation="softmax"))
+
+model_keras_2h.compile(optimizer=keras.optimizers.Adam(lr = 0.001), 
+                       loss='categorical_crossentropy',
+                       metrics=['accuracy'])
+
+# Train
+history = model_keras_2h.fit(X_train, y_train, 
+                             batch_size=minibatch_size, epochs=num_epochs,
+                             validation_data=(X_valid, y_valid))
+
+# Visualization
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+        
+plt.figure(figsize=(8, 8))
+plt.subplot(2, 1, 1)
+plt.plot(acc, label='Training Accuracy')
+plt.plot(val_acc, label='Validation Accuracy')
+plt.legend(loc='lower right')
+plt.ylabel('Accuracy')
+plt.ylim([min(plt.ylim()),1])
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(2, 1, 2)
+plt.plot(loss, label='Training Loss')
+plt.plot(val_loss, label='Validation Loss')
+plt.legend(loc='upper right')
+plt.ylabel('Cross Entropy')
+plt.ylim([0,1.0])
+plt.title('Training and Validation Loss')
+plt.xlabel('epoch')
+plt.show()
+
+# Evaluation
+accuracy_test = model_keras_2h.evaluate(X_test, y_test)
+print('Test Accuracy - ', accuracy_test[1])
+
+y_pred = model_keras_2h.predict(X_test)
+macro_auc = metrics.roc_auc_score(y_test,y_pred)
+print('Test Macro-AUC - ', macro_auc)
