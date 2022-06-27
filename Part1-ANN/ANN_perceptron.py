ee6a18942ffd83084cdfc037636a5f78bbad7fa3
Idan-Kassis <93663187+Idan-Kassis@users.noreply.github.com>
2022-06-27 17:01:43 +0300

    Add files via upload

---
+++ Part1-ANN/ANN_perceptron.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 18 18:55:56 2022
+
+@author: Dror
+"""
+# Setup
+import numpy as np
+
+def sigmoid(z):                                        
+    return 1. / (1. + np.exp(-z))
+
+
+def int_to_onehot(y, num_labels):
+
+    ary = np.zeros((y.shape[0], num_labels))
+    for i, val in enumerate(y):
+        ary[i, val] = 1
+
+    return ary
+
+
+class NeuralNetMLP_2h:
+
+    def __init__(self, num_features, num_hidden, num_classes, random_seed=123):
+        super().__init__()
+        
+        self.num_classes = num_classes
+        
+        # hidden
+        rng = np.random.RandomState(random_seed)
+        
+        self.weight_h1 = rng.normal(
+            loc=0.0, scale=0.1, size=(num_hidden, num_features))
+        self.bias_h1 = np.zeros(num_hidden)
+        
+        # output 1
+        self.weight_h2 = rng.normal(
+            loc=0.0, scale=0.1, size=(num_hidden, num_hidden))
+        self.bias_h2 = np.zeros(num_hidden)
+        
+        # output 2
+        self.weight_out = rng.normal(
+            loc=0.0, scale=0.1, size=(num_classes, num_hidden))
+        self.bias_out = np.zeros(num_classes)
+        
+    def forward(self, x):
+        # Hidden layer
+        # input dim: [n_examples, n_features] dot [n_hidden, n_features].T
+        # output dim: [n_examples, n_hidden]
+        z_h1 = np.dot(x, self.weight_h1.T) + self.bias_h1
+        a_h1 = sigmoid(z_h1)
+
+        # Output 1st layer
+        # input dim: [n_examples, n_hidden] dot [n_classes, n_hidden].T
+        # output dim: [n_examples, n_classes]
+        z_h2 = np.dot(a_h1, self.weight_h2.T) + self.bias_h2
+        a_h2 = sigmoid(z_h2)
+        
+        # Output 2nd layer
+        z_out = np.dot(a_h2, self.weight_out.T) +self.bias_out
+        a_out = sigmoid(z_out)
+        
+        return a_h1, a_h2, a_out
+
+    def backward(self, x, a_h1, a_h2, a_out, y):  
+    
+        #########################
+        ### Output layer weights
+        #########################
+        
+        # onehot encoding
+        y_onehot = int_to_onehot(y, self.num_classes)
+
+        # Part 1: dLoss/dOutWeights
+        ## = dLoss/dOutAct * dOutAct/dOutNet * dOutNet/dOutWeight
+        ## where DeltaOut = dLoss/dOutAct * dOutAct/dOutNet
+        ## for convenient re-use
+        
+        # input/output dim: [n_examples, n_classes]
+        d_loss__d_a_out = 2.*(a_out - y_onehot) / y.shape[0]
+        # input/output dim: [n_examples, n_classes]
+        d_a_out__d_z_out = a_out * (1. - a_out) # sigmoid derivative
+        # output dim: [n_examples, n_classes]
+        delta_out = d_loss__d_a_out * d_a_out__d_z_out # "delta (rule) placeholder"
+        # gradient for output weights
+        
+        # [n_examples, n_hidden]
+        d_z_out__dw_out = a_h2
+        # input dim: [n_classes, n_examples] dot [n_examples, n_hidden]
+        # output dim: [n_classes, n_hidden]
+        d_loss__dw_out = np.dot(delta_out.T, d_z_out__dw_out)
+        d_loss__db_out = np.sum(delta_out, axis=0)
+        
+
+        #################################        
+        # Part 2: dLoss/dHidden2Weights
+        ## = DeltaOut * dOutNet/dHiddenAct * dHiddenAct/dHiddenNet * dHiddenNet/dWeight
+        
+        # [n_hidden, n_hidden]
+        d_z_out__a_h2 = self.weight_out
+        
+        # output dim: [n_examples, n_hidden]
+        d_loss__a_h2 = np.dot(delta_out, d_z_out__a_h2)
+
+        # [n_examples, n_hidden]
+        d_a_h__d_z_h2 = a_h2 * (1. - a_h2) # sigmoid derivative
+        
+        # [n_examples, n_features]
+        d_z_h__d_w_h2 = a_h1
+        
+        # output dim: [n_hidden, n_features]
+        d_loss__d_w_h2 = np.dot((d_loss__a_h2 * d_a_h__d_z_h2).T, d_z_h__d_w_h2)
+        d_loss__d_b_h2 = np.sum((d_loss__a_h2 * d_a_h__d_z_h2), axis=0)
+        
+        ##################################
+        # Part 3: dLoss/dHidden1Weights
+        ## = DeltaOut * dOutNet/dHiddenAct * dHiddenAct/dHiddenNet * dHiddenNet/dWeight
+        
+        # [n_classes, n_hidden]
+        d_z_out__a_h1 = self.weight_h2
+        
+        # output dim: [n_examples, n_hidden]
+        d_loss__a_h1 = np.dot((d_loss__a_h2 * d_a_h__d_z_h2), d_z_out__a_h1)
+        
+        # [n_examples, n_hidden]
+        d_a_h__d_z_h1 = a_h1 * (1. - a_h1) # sigmoid derivative
+        
+        # [n_examples, n_features]
+        d_z_h__d_w_h1 = x    
+        
+        
+        # output dim: [n_hidden, n_features]
+        d_loss__d_w_h1 = np.dot((d_loss__a_h1 * d_a_h__d_z_h1).T, d_z_h__d_w_h1)
+        d_loss__d_b_h1 = np.sum((d_loss__a_h1 * d_a_h__d_z_h1), axis=0)
+        return (d_loss__dw_out, d_loss__db_out, 
+                d_loss__d_w_h2, d_loss__d_b_h2,
+                d_loss__d_w_h1, d_loss__d_b_h1)
+    
+
+class NeuralNetMLP_1h:
+
+    def __init__(self, num_features, num_hidden, num_classes, random_seed=123):
+        super().__init__()
+        
+        self.num_classes = num_classes
+        
+        # hidden
+        rng = np.random.RandomState(random_seed)
+        
+        self.weight_h = rng.normal(
+            loc=0.0, scale=0.1, size=(num_hidden, num_features))
+        self.bias_h = np.zeros(num_hidden)
+        
+        # output
+        self.weight_out = rng.normal(
+            loc=0.0, scale=0.1, size=(num_classes, num_hidden))
+        self.bias_out = np.zeros(num_classes)
+        
+    def forward(self, x):
+        # Hidden layer
+        # input dim: [n_examples, n_features] dot [n_hidden, n_features].T
+        # output dim: [n_examples, n_hidden]
+        z_h = np.dot(x, self.weight_h.T) + self.bias_h
+        a_h = sigmoid(z_h)
+
+        # Output layer
+        # input dim: [n_examples, n_hidden] dot [n_classes, n_hidden].T
+        # output dim: [n_examples, n_classes]
+        z_out = np.dot(a_h, self.weight_out.T) + self.bias_out
+        a_out = sigmoid(z_out)
+        return a_h, a_out
+
+    def backward(self, x, a_h, a_out, y):  
+    
+        #########################
+        ### Output layer weights
+        #########################
+        
+        # onehot encoding
+        y_onehot = int_to_onehot(y, self.num_classes)
+
+        # Part 1: dLoss/dOutWeights
+        ## = dLoss/dOutAct * dOutAct/dOutNet * dOutNet/dOutWeight
+        ## where DeltaOut = dLoss/dOutAct * dOutAct/dOutNet
+        ## for convenient re-use
+        
+        # input/output dim: [n_examples, n_classes]
+        d_loss__d_a_out = 2.*(a_out - y_onehot) / y.shape[0]
+
+        # input/output dim: [n_examples, n_classes]
+        d_a_out__d_z_out = a_out * (1. - a_out) # sigmoid derivative
+
+        # output dim: [n_examples, n_classes]
+        delta_out = d_loss__d_a_out * d_a_out__d_z_out # "delta (rule) placeholder"
+
+        # gradient for output weights
+        
+        # [n_examples, n_hidden]
+        d_z_out__dw_out = a_h
+        
+        # input dim: [n_classes, n_examples] dot [n_examples, n_hidden]
+        # output dim: [n_classes, n_hidden]
+        d_loss__dw_out = np.dot(delta_out.T, d_z_out__dw_out)
+        d_loss__db_out = np.sum(delta_out, axis=0)
+        
+
+        #################################        
+        # Part 2: dLoss/dHiddenWeights
+        ## = DeltaOut * dOutNet/dHiddenAct * dHiddenAct/dHiddenNet * dHiddenNet/dWeight
+        
+        # [n_classes, n_hidden]
+        d_z_out__a_h = self.weight_out
+        
+        # output dim: [n_examples, n_hidden]
+        d_loss__a_h = np.dot(delta_out, d_z_out__a_h)
+        
+        # [n_examples, n_hidden]
+        d_a_h__d_z_h = a_h * (1. - a_h) # sigmoid derivative
+        
+        # [n_examples, n_features]
+        d_z_h__d_w_h = x
+        
+        # output dim: [n_hidden, n_features]
+        d_loss__d_w_h = np.dot((d_loss__a_h * d_a_h__d_z_h).T, d_z_h__d_w_h)
+        d_loss__d_b_h = np.sum((d_loss__a_h * d_a_h__d_z_h), axis=0)
+
+        return (d_loss__dw_out, d_loss__db_out, 
+                d_loss__d_w_h, d_loss__d_b_h)
